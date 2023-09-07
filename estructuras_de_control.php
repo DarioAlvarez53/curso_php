f83d089f96b31aed45057942e9d02a70c920087d
Dario Alvarez <daralv53@gmail.com>
2023-09-07 00:30:03 -0600

    Estructuras de datos parte 2

--- estructuras_de_control.php
+++ estructuras_de_control.php
@@ -30,9 +30,28 @@ do {
 
 // Estructura de control "for/foreach", esto es un ciclo que permite pasar por un array en cada una de sus posiciones
 
-$arr = array(1,2,3,4);
-for($i=0; $i<4; $i++) {
-    echo $arr[$i];
+// $arr = array(1,2,3,4);
+// for($i=0; $i<4; $i++) {
+//     echo $arr[$i];
+// }
+
+// Estructura de control "switch", se tendran diferentes casos para una variable,esto quiere decir que si uno de estos casos se llega a cumplir entonces se va a ejecutar la instruccion dada
+
+$valorA = 2;
+
+switch($valorA) {
+    case 1:
+        echo "El valor es 1";
+    break;
+    case 2:
+        echo "El valor es 2";
+    break;
+    case 3:
+        echo "El valor es 3";
+    break;
+    default:
+        echo "No es ninguno de los valores 1, 2 o 3";
+    break;
 }
 
 ?>
