2750d08c3e890485869cb6ed0490f6c00d839020
Dario Alvarez <daralv53@gmail.com>
2023-09-07 01:10:52 -0600

    Funciones matematicas

---
+++ Functions/funciones-matematicas.php
@@ -0,0 +1,14 @@
+<?php
+
+// echo max(9,4,3,2,6); // nos da el numro mas grande 
+// echo min(9,4,3,2,6); // nos da el numero mas pequeño
+
+// echo pi(); // nos da el valor de pi
+
+// echo rand(); // nos genera un numero aleatorio, se le especifican dos rangos, min y max
+
+// echo sqrt(5); // nos da la raiz cuadrada de un numero
+
+
+
+?>
