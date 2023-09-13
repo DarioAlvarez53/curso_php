0c409370c18282ac33e55cb60197b5d97976d483
Dario <dario_bromista@hotmail.com>
2023-09-13 09:35:16 -0600

    Operadores aritmeticos

---
+++ operadores_aritmeticos.php
@@ -0,0 +1,44 @@
+<?php
+$valorA = 5;
+$valorB = 10;
+
+//Operador suma:
+/*
+$suma = $valorA + $valorB;
+
+var_dump($suma);
+*/
+
+//Operdor resta
+/*
+$resta = $valorA - $valorB;
+
+var_dump($resta);
+*/
+
+//Operador de multiplicacion
+/*
+$multiplicacion = $valorA * $valorB;
+
+var_dump($multiplicacion);
+*/
+
+//Operador de division
+/*
+$division = $valorA / $valorB;
+
+var_dump($division);
+*/
+
+//Operador de modulo
+/*
+$modulo = $valorA % $valorB;
+var_dump($modulo);
+*/
+
+//Operador de potenciacion
+/*
+$potenciacion = $valorA ** $valorB;
+var_dump($potenciacion);
+*/
+?>
